b3ab4bb8012d4f6d987923a08cd9e25a855f1503
Charlie Austin <Charlie_Austin@gap.com>
2012-11-09 10:50:20 -0800

    fixed the error with amount not getting updated

--- knockout-ex.js
+++ knockout-ex.js
@@ -1,37 +1,35 @@
-
 function FareCalculationModel() {
 
     this.stations = [
-        { stationName: "12th"},
-        { stationName: "16th"},
-        { stationName: "19th"}
-    ];    
+        { stationName:"12th"},
+        { stationName:"16th"},
+        { stationName:"19th"}
+    ];
 
     this.originStation = ko.observable(this.stations[0]);
 
     this.destinationStation = ko.observable(this.stations[1]);
 
-    this.formattedPrice = ko.computed(function(){
+    this.formattedPrice = ko.computed(function () {
         //console.log(this.originStation() + this.destinationStation());
         var fare = "n/a";
         $.ajax({
-            type: "GET",
-            dataType: "xml",
-            url: "http://api.bart.gov/api/sched.aspx?cmd=fare&orig="+this.originStation().stationName+"&dest="+this.destinationStation().stationName+"&key=MW9S-E7SL-26DU-VV8V",
-            success: function(xml)  {
+            async:false,
+            type:"GET",
+            dataType:"xml",
+            url:"http://api.bart.gov/api/sched.aspx?cmd=fare&orig=" + this.originStation().stationName + "&dest=" + this.destinationStation().stationName + "&key=MW9S-E7SL-26DU-VV8V",
+            success:function (xml) {
                 console.log(xml);
-                $(xml).find("trip").each(function(){
+                $(xml).find("trip").each(function () {
                     fare = $(this).find('fare').text();
-                    alert(fare);
-
                 });
             }
         });
         return fare;
-        },this); 
-    }    
+    }, this);
+}
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     ko.applyBindings(new FareCalculationModel());
 });
